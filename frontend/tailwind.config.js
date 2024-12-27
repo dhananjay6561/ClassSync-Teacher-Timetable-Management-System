5df7ed0c54d732c230a97a27119a09db183ac95b
Dhananjay Aggarwal <dhananjayaggarwal6561@gmail.com>
2024-12-27 20:59:16 +0530

    fixed tailwind issue

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Include all files in the src directory
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
